95a6a3d103a93aa662a689efb1725c02a07ca52f
omar1vega <omar1vega@gmail.com>
2018-11-07 23:46:09 -0800

    added firebase.py

---
+++ shelf/firebase.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+def updateDistance(distance):
+    firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
+                                  {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
+    root = db.reference('distance')
+    root.update({
+        'distance': distance
+    })
