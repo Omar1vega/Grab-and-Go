b1afdb73e1d232fa3237dfa73971519dbd680407
vegao1 <vegao1@uci.edu>
2018-11-15 00:45:09 -0800

    Added Distance model class

---
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/models/Distance.java
@@ -0,0 +1,12 @@
+package com.amazotgo.storeapp.models;
+
+public class Distance {
+    public int distance;
+
+    public Distance() {
+    }
+
+    public Distance(int distance) {
+        this.distance = distance;
+    }
+}
