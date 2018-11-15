5433dcca3695f096963de12d02b69917d85f596c
vegao1 <vegao1@uci.edu>
2018-11-14 22:57:31 -0800

    Added firebase reference to MainActivity

--- storeapp/app/src/main/java/com/amazotgo/storeapp/MainActivity.java
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.amazotgo.storeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    private TextView distanceContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        distanceContainer = findViewById(R.id.distance_container);
+
+        setTitle(R.string.app_name);
         setContentView(R.layout.activity_main);
     }
 }
