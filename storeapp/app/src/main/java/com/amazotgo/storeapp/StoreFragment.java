dd41da9580e14f0ef2ce5ada8558a8b108f4009c
vegao1 <vegao1@uci.edu>
2018-12-23 00:50:40 -0800

    added store fragment

---
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/StoreFragment.java
@@ -0,0 +1,24 @@
+package com.amazotgo.storeapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StoreFragment extends Fragment {
+
+
+    public StoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_store, container, false);
+    }
+
+}
