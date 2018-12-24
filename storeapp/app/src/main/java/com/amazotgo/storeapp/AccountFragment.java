ddc4c91fbc7fdf329f2379f5c7871b321c300899
vegao1 <vegao1@uci.edu>
2018-12-23 22:14:00 -0800

    moved current username textview to account fragment

--- storeapp/app/src/main/java/com/amazotgo/storeapp/AccountFragment.java
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/AccountFragment.java
@@ -8,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AccountFragment extends Fragment {
     private Button signOutButton;
     private Main2Activity main;
+    private TextView authenticatedUser;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -22,6 +24,7 @@ public class AccountFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
         main = (Main2Activity) getActivity();
+
         signOutButton = view.findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +32,11 @@ public class AccountFragment extends Fragment {
                 main.signOut();
             }
         });
+
+        authenticatedUser = view.findViewById(R.id.auth_user_name);
+        if (main.getAuth().getCurrentUser().getDisplayName() != null) {
+            authenticatedUser.setText(main.getAuth().getCurrentUser().getDisplayName());
+        }
         return view;
     }
-
 }
