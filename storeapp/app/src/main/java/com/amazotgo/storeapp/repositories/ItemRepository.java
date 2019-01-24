d88f5c924e827ced9d9c5a3d4b18816733e59420
vegao1 <vegao1@uci.edu>
2019-01-24 00:02:06 -0800

    removed dummy setItems from ItemRepository

--- storeapp/app/src/main/java/com/amazotgo/storeapp/repositories/ItemRepository.java
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/repositories/ItemRepository.java
@@ -25,46 +25,10 @@ public class ItemRepository {
 
 
     // Pretend to get data from a webservice or online source
-    public MutableLiveData<List<Item>> getNicePlaces() {
-        setNicePlaces();
+    public MutableLiveData<List<Item>> getItems() {
         MutableLiveData<List<Item>> data = new MutableLiveData<>();
         data.setValue(dataSet);
         return data;
-    }
-
-    private void setNicePlaces() {
-        dataSet.add(
-                new Item("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg",
-                        "Havasu Falls")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/tpsnoz5bzo501.jpg",
-                        "Trondheim")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/qn7f9oqu7o501.jpg",
-                        "Portugal")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/j6myfqglup501.jpg",
-                        "Rocky Mountain National Park")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/0h2gm1ix6p501.jpg",
-                        "Havasu Falls")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/k98uzl68eh501.jpg",
-                        "Mahahual")
-        );
-        dataSet.add(
-                new Item("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg",
-                        "Frozen Lake")
-        );
-        dataSet.add(
-                new Item("https://i.redd.it/obx4zydshg601.jpg",
-                        "Austrailia")
-        );
     }
 }
 
