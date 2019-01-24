a94eda833e3250cc8900bfe52cc6c3f0dbc044e0
vegao1 <vegao1@uci.edu>
2019-01-24 00:01:16 -0800

    added noargs constructor and equals/hash override to Item

--- storeapp/app/src/main/java/com/amazotgo/storeapp/models/Item.java
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/models/Item.java
@@ -1,8 +1,13 @@
 package com.amazotgo.storeapp.models;
 
+import java.util.Objects;
+
 public class Item {
     private String imageUrl;
     private String name;
+
+    public Item() {
+    }
 
     public Item(String imageUrl, String name) {
         this.imageUrl = imageUrl;
@@ -23,5 +28,19 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(imageUrl, item.imageUrl) &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(imageUrl, name);
     }
 }
