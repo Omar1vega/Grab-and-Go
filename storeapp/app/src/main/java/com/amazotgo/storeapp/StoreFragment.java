c70f5e0f44d9cf3d02fd3a54c7f59b112f52fdd4
vegao1 <vegao1@uci.edu>
2019-01-23 23:19:14 -0800

    added recyclerview to store fragment java

--- storeapp/app/src/main/java/com/amazotgo/storeapp/StoreFragment.java
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/StoreFragment.java
@@ -1,9 +1,14 @@
 package com.amazotgo.storeapp;
 
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,21 +18,31 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazotgo.storeapp.adapters.RecyclerAdapter;
 import com.amazotgo.storeapp.models.Distance;
+import com.amazotgo.storeapp.models.Item;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
+
 public class StoreFragment extends Fragment {
     public final String TAG = this.getClass().getCanonicalName();
+
+    private StoreFragmentViewModel storeFragmentViewModel;
+
     private Main2Activity main;
     private DatabaseReference mDatabase;
     private DatabaseReference mDistanceReference;
 
     private TextView distanceContainer;
     private Animation blink;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
 
 
     public StoreFragment() {
@@ -44,7 +59,19 @@ public class StoreFragment extends Fragment {
         mDistanceReference = mDatabase.child("distance");
 
         distanceContainer = view.findViewById(R.id.distance_container);
+        mRecyclerView = view.findViewById(R.id.item_list);
         blink = AnimationUtils.loadAnimation(main, R.anim.blink);
+
+        storeFragmentViewModel = ViewModelProviders.of(this).get(StoreFragmentViewModel.class);
+        storeFragmentViewModel.init();
+        storeFragmentViewModel.getNicePlaces().observe(this, new Observer<List<Item>>() {
+            @Override
+            public void onChanged(@Nullable List<Item> items) {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        initRecyclerView();
         return view;
     }
 
@@ -69,6 +96,12 @@ public class StoreFragment extends Fragment {
                 Toast.makeText(main, "Failed to load distance.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void initRecyclerView() {
+        mAdapter = new RecyclerAdapter(this.getContext(), storeFragmentViewModel.getNicePlaces().getValue());
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
