7b8632deee0d491599bee8916d91fc8cc2d3dacd
vegao1 <vegao1@uci.edu>
2019-01-23 23:17:38 -0800

    added item model

---
+++ storeapp/app/src/main/java/com/amazotgo/storeapp/models/Item.java
@@ -0,0 +1,27 @@
+package com.amazotgo.storeapp.models;
+
+public class Item {
+    private String imageUrl;
+    private String name;
+
+    public Item(String imageUrl, String name) {
+        this.imageUrl = imageUrl;
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
