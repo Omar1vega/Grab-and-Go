16ed8ff390be2edc4c9f79a9bab1abfcc0b55ddf
vegao1 <vegao1@uci.edu>
2019-02-11 19:24:45 -0800

    formatting

--- shelf/shelf.py
+++ shelf/shelf.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-########################################################################
-# Filename    : shelf.py
-# Description : Detecting item pickup at shelf
-# Author      : vegao1
-# modification: 2018/11/07
-########################################################################
 
 import time
 
@@ -19,71 +13,61 @@ MAX_DISTANCE = 220  # define the maximum measured distance
 timeOut = MAX_DISTANCE * 60  # calculate timeout according to the maximum measured distance
 
 firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
-                              {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
+							  {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
 root = db.reference('distance')
 cart = db.reference("carts/8mBk742Op7cpW2RYZkb4yRoWpN92")
 
-item = {'name':'LaCroix', "imageUrl":"https://images-na.ssl-images-amazon.com/images/I/51ans2c7qUL.jpg"}
+item = {'name': 'LaCroix', "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/51ans2c7qUL.jpg"}
 
 itemKey = ''
 
+
 def addItem():
-    items = cart.child("items")
-    newItemKey = items.push()
+	newItemKey = cart.child("items").push()
 
-    global itemKey
-    itemKey = newItemKey
+	global itemKey
+	itemKey = newItemKey
 
-    newItemKey.set(item)
+	newItemKey.set(item)
+
 
 def removeItem():
-    global itemKey
-    itemKey.delete()
-
-#    print("itemKey = ", itemKey)
-#    itemRef = cart.child("items").child(str(itemKey))
-#    itemRef.remove()
+	global itemKey
+	itemKey.delete()
 
 
 def updateDistance(distance):
-    root.update({
-        'distance': distance
-    })
+	root.update({
+		'distance': distance
+	})
 
 
 def pulseIn(pin, level, timeOut):  # function pulseIn: obtain pulse time of a pin
-    t0 = time.time()
-    while GPIO.input(pin) != level:
-        if (time.time() - t0) > timeOut * 0.000001:
-            return 0
-    t0 = time.time()
-    while GPIO.input(pin) == level:
-        if (time.time() - t0) > timeOut * 0.000001:
-            return 0
-    return (time.time() - t0) * 1000000
+	t0 = time.time()
+	while GPIO.input(pin) != level:
+		if (time.time() - t0) > timeOut * 0.000001:
+			return 0
+	t0 = time.time()
+	while GPIO.input(pin) == level:
+		if (time.time() - t0) > timeOut * 0.000001:
+			return 0
+	return (time.time() - t0) * 1000000
 
 
 def getDistance():  # get the measurement results of ultrasonic module,with unit: cm
-    GPIO.output(trigPin, GPIO.HIGH)  # make trigPin send 10us high level
-    time.sleep(0.00001)  # 10us
-    GPIO.output(trigPin, GPIO.LOW)
-    pingTime = pulseIn(echoPin, GPIO.HIGH, timeOut)  # read plus time of echoPin
-    distance = pingTime * 340.0 / 2.0 / 10000.0  # the sound speed is 340m/s, and calculate distance
-    return distance
+	GPIO.output(trigPin, GPIO.HIGH)  # make trigPin send 10us high level
+	time.sleep(0.00001)  # 10us
+	GPIO.output(trigPin, GPIO.LOW)
+	pingTime = pulseIn(echoPin, GPIO.HIGH, timeOut)  # read plus time of echoPin
+	distance = pingTime * 340.0 / 2.0 / 10000.0  # the sound speed is 340m/s, and calculate distance
+	return distance
 
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(trigPin, GPIO.OUT)
-    GPIO.setup(echoPin, GPIO.IN)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(trigPin, GPIO.OUT)
+	GPIO.setup(echoPin, GPIO.IN)
 
-
-def checkDistance(distance):
-    return 20 >= distance > 0
-
-
-personAlerted = False
-counter = 0
 
 itemPresent = True
 itemAdded = False
@@ -91,48 +75,42 @@ itemRemoved = False
 
 
 def awayMode():
-    global itemPresent
-    global itemAdded
-    global itemRemoved
+	global itemPresent, itemAdded, itemRemoved
 
-    distance = getDistance()
-    print("distance =", distance)
+	distance = getDistance()
+	print("distance =", distance)
 
-    updateDistance(int(distance))
+	if (37 - 4 < distance < 37 + 4):
+		itemPresent = True
 
-    if(distance > 37-4 and distance < 37+4):
-        itemPresent = True
+	if (distance > 54 - 4):
+		itemPresent = False
 
-    if(distance > 54 -4  ):
-        itemPresent = False
+	if (not itemPresent and not itemAdded):
+		addItem()
+		itemAdded = True
 
-    if(not itemPresent and not itemAdded):
-        addItem()
-        itemAdded = True
+	if (itemPresent and not itemRemoved and itemAdded):
+		removeItem()
+		itemRemoved = True
+		itemAdded = False
 
-    if(itemPresent and not itemRemoved and itemAdded):
-        removeItem()
-        itemRemoved = True
-        itemAdded = False
-
-
-
-    time.sleep(0.5) 
+	time.sleep(0.5)
 
 
 def loop():
-    while True:
-        awayMode()
+	while True:
+		awayMode()
 
 
 def destroy():
-    GPIO.cleanup()
+	GPIO.cleanup()
 
 
 if __name__ == '__main__':
-    print('Program is starting ... ')
-    setup()
-    try:
-        loop()
-    except KeyboardInterrupt:
-        destroy()
+	print('Program is starting ... ')
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:
+		destroy()
