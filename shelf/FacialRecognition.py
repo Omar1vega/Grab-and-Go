66dd8524e9a531d7ee64b2224b8b33e4009afad6
vegao1 <vegao1@uci.edu>
2019-02-14 22:58:47 -0800

    added uploadToS3

--- shelf/FacialRecognition.py
+++ shelf/FacialRecognition.py
@@ -1,6 +1,8 @@
 import subprocess
 from datetime import datetime
 
+import boto3
+
 
 def takePicture():
     filename = datetime.now().strftime('%Y_%m_%d__%H_%M_%S') + ".jpg"
@@ -8,10 +10,18 @@ def takePicture():
     return filename
 
 
+def uploadToS3(filepath):
+    s3 = boto3.resource('s3')
+    uploadPath = "pickups" + filepath.split("/")[-1]
+    s3.meta.client.upload_file(filepath, 'amazotgo', uploadPath)
+    return uploadPath
+
+
 if __name__ == '__main__':
     while True:
         picture = takePicture()
         if picture:
             print(picture)
+            print(uploadToS3(picture))
         else:
             print("Failed to take picture")
