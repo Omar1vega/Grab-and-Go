9b65bfac9b7b3937aeed29df08a2883408bce4fe
vegao1 <vegao1@uci.edu>
2019-02-14 21:00:32 -0800

    replaced cv2 with subprocess call to fswebcam

--- shelf/FacialRecognition.py
+++ shelf/FacialRecognition.py
@@ -1,27 +1,17 @@
+import subprocess
 from datetime import datetime
 
-import cv2
 
-
-def takePicture(camera):
-    read, frame = camera.read()
-
-    if read:
-        filename = datetime.now().strftime('%Y_%m_%d__%H_%M_%S') + ".png"
-        cv2.imwrite(filename, frame)
-        return filename
-    return False
+def takePicture():
+    filename = datetime.now().strftime('%Y_%m_%d__%H_%M_%S') + ".jpg"
+    subprocess.call("fswebcam -r 1280x720 --no-banner " + filename, shell=True)
+    return filename
 
 
 if __name__ == '__main__':
     while True:
-        camera = cv2.VideoCapture(0)
-        try:
-            picture = takePicture(camera)
-            camera.release()
-            if picture:
-                print(picture)
-            else:
-                print("Failed to take picture")
-        except KeyboardInterrupt:
-            camera.release()
+        picture = takePicture()
+        if picture:
+            print(picture)
+        else:
+            print("Failed to take picture")
