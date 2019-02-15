939b3da4ac7284765deace05f311e240f7e7ed61
vegao1 <vegao1@uci.edu>
2019-02-14 19:24:29 -0800

    added dependecy injection to takePicture()

--- shelf/FacialRecognition.py
+++ shelf/FacialRecognition.py
@@ -1,20 +1,24 @@
+from datetime import datetime
+
 import cv2
 
 
-def takePicture():
-    camera = cv2.VideoCapture(0)
+def takePicture(camera):
     read, frame = camera.read()
+    camera.release()
 
     if read:
-        filename = "pickup.png"
+        filename = datetime.now().strftime('%Y_%m_%d__%H_%M_%S') + ".png"
         cv2.imwrite(filename, frame)
         return filename
     return False
 
 
 if __name__ == '__main__':
-    picture = takePicture()
-    if picture:
-        print(picture)
-    else:
-        print("Failed to take picture")
+    while True:
+        camera = cv2.VideoCapture(0)
+        picture = takePicture(camera)
+        if picture:
+            print(picture)
+        else:
+            print("Failed to take picture")
