d0fb5e25c84e2316799e7c7aecca84ccf42c4b48
vegao1 <vegao1@uci.edu>
2019-03-07 21:47:42 -0800

    added base Calibrate script

---
+++ shelf/Calibrate.py
@@ -0,0 +1,57 @@
+from collections import deque
+from random import randint
+
+from shelf import getDistance, setup
+
+
+def calibrate():
+    setup()
+    d = deque(maxlen=10)
+    while True:
+        distance = int(getDistance())
+        print("Distance: " + str(distance))
+        d.append(distance)
+        stability = stable(d)
+        if stability != 0:
+            return stability
+
+
+def main():
+    d = deque(maxlen=10)
+
+    start = 28
+    end = 32
+
+    for i in range(500):
+        random_number = randint(start, end)
+        d.append(random_number)
+
+        stability = stable(d)
+        if stability != 0:
+            print("Data is stable with average: " + str(stability) + " after " + str(i) + " iterations")
+
+            print("Final values:")
+            for number in d:
+                print(number)
+            return
+
+
+def stable(dq):
+    if len(dq) != dq.maxlen:
+        return 0
+
+    total = 0
+    for num in dq:
+        total += num
+    average = total // len(dq)
+
+    for num in dq:
+        if num > (average + 1) or num < (average - 1):
+            return 0
+
+    return average
+
+
+if __name__ == '__main__':
+    stability = calibrate()
+    print("Stable at " + str(stability))
