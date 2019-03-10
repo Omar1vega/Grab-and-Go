9238eff47f2f835e3477dea286b0f77b8790152a
vegao1 <vegao1@uci.edu>
2019-03-10 01:12:04 -0800

    converted firebase to class

--- shelf/firebase.py
+++ shelf/firebase.py
@@ -2,18 +2,22 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
-                                  {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
-root = db.reference('distance')
 
-print(root.get())
- 
+class Firebase:
+    def __init__(self):
+        firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
+                                      {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
+        self.cache = {}
 
+    def get_items(self):
+        return list(db.reference('items').get().values())
 
-def updateDistance(distance):
-    firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
-                                  {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
-    root = db.reference('distance')
-    root.update({
-        'distance': distance
-    })
+    def add_item(self, item, user):
+        new_item_key = db.reference("carts/" + user).child("items").push()
+        new_item_key.set(item.get_value())
+
+        self.cache[user] = new_item_key
+
+    def remove_item(self, user):
+        self.cache[user].delete()
+        del self.cache[user]
