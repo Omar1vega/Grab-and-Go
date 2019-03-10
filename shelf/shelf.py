992c13a006c56bbd96cc64fbdb8274aea236e685
vegao1 <vegao1@uci.edu>
2019-03-10 01:43:13 -0800

    added delay after empty calibration

--- shelf/shelf.py
+++ shelf/shelf.py
@@ -2,77 +2,22 @@
 
 import time
 
-import RPi.GPIO as GPIO
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-
 from FacialRecognition import takePicture, uploadToS3, recognize
-
-trigPin = 18
-echoPin = 22
-MAX_DISTANCE = 220  # define the maximum measured distance
-timeOut = MAX_DISTANCE * 60  # calculate timeout according to the maximum measured distance
 
 itemPresent = True
 itemAdded = False
 itemRemoved = False
 
-firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
-                              {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
-root = db.reference('distance')
-cart = db.reference("carts/8mBk742Op7cpW2RYZkb4yRoWpN92")
 
-item = {'name': 'LaCroix', "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/51ans2c7qUL.jpg"}
+class Shelf:
+    def __init__(self, item, sensor, camera, display):
+        self.item = item
+        self.sensor = sensor
+        self.camera = camera
+        self.display = display
 
-itemKey = ''
-
-
-def add_item(user):
-    new_item_key = db.reference("carts/"+user).child("items").push()
-
-    global itemKey
-    itemKey = new_item_key
-
-    new_item_key.set(item)
-
-
-def removeItem():
-    global itemKey
-    itemKey.delete()
-
-
-def updateDistance(distance):
-    root.update({
-        'distance': distance
-    })
-
-
-def pulseIn(pin, level, timeOut):  # function pulseIn: obtain pulse time of a pin
-    t0 = time.time()
-    while GPIO.input(pin) != level:
-        if (time.time() - t0) > timeOut * 0.000001:
-            return 0
-    t0 = time.time()
-    while GPIO.input(pin) == level:
-        if (time.time() - t0) > timeOut * 0.000001:
-            return 0
-    return (time.time() - t0) * 1000000
-
-
-def getDistance():  # get the measurement results of ultrasonic module,with unit: cm
-    GPIO.output(trigPin, GPIO.HIGH)  # make trigPin send 10us high level
-    time.sleep(0.00001)  # 10us
-    GPIO.output(trigPin, GPIO.LOW)
-    pingTime = pulseIn(echoPin, GPIO.HIGH, timeOut)  # read plus time of echoPin
-    distance = pingTime * 340.0 / 2.0 / 10000.0  # the sound speed is 340m/s, and calculate distance
-    return distance
-
-
-def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(trigPin, GPIO.OUT)
-    GPIO.setup(echoPin, GPIO.IN)
+    def set_item(self, item):
+        self.item = item
 
 
 def main():
@@ -95,7 +40,6 @@ def main():
                 print(s3Filepath)
                 id, name = recognize(s3Filepath)
 
-
                 add_item(id)
             itemAdded = True
 
@@ -105,10 +49,6 @@ def main():
             itemAdded = False
 
         time.sleep(0.5)
-
-
-def destroy():
-    GPIO.cleanup()
 
 
 if __name__ == '__main__':
