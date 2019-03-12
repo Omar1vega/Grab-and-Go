1c80cb0ae9123c9f4b094bea6697df275bbce360
vegao1 <vegao1@uci.edu>
2019-03-11 23:07:34 -0700

    updated remove_item in Firebase

--- shelf/Firebase.py
+++ shelf/Firebase.py
@@ -7,7 +7,6 @@ class Firebase:
     def __init__(self):
         firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'),
                                       {'databaseURL': 'https://androidsample-225db.firebaseio.com/'})
-        self.cache = {}
 
     def get_items(self):
         return list(db.reference('items').get().values())
@@ -16,8 +15,11 @@ class Firebase:
         new_item_key = db.reference("carts/" + user).child("items").push()
         new_item_key.set(item)
 
-        self.cache[user] = new_item_key
-
-    def remove_item(self, user):
-        self.cache[user].delete()
-        del self.cache[user]
+    def remove_item(self, item, user):
+        item_reference = db.reference("carts/" + user).child("items")
+        items = item_reference.get()
+        for key in items:
+            item_in_cart = items[key]
+            if item_in_cart == item:
+                item_reference.child(key).delete()
+                return
